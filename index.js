da97b65200266e35f191dd8d0f14634df3fa63c4
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-27 16:57:54 -0500

    Continuing in following the guide, set up the connection, both server and client are connected, client can send emssages

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const { Socket } = require("engine.io");
 const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    console.log("A user connected!!!");
+    socket.on('chat message', (msg) => {
+        console.log("The user typed: " + msg);
+    }) 
+    socket.on("disconnect", () => +
+    console.log("A user has disconnected :/ "));
+})
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
